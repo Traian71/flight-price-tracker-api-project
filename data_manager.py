a9592140fae72bd6c580c370cfbe46a4df40eb15
Traian <150020589+Traian71@users.noreply.github.com>
2024-10-19 15:28:57 +0300

    Add files via upload

---
+++ data_manager.py
@@ -0,0 +1,29 @@
+from pprint import pprint
+import requests
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/60e39847012dd08b73bbc7a0447ace5b/flightDeals/prices"
+
+
+class DataManager:
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            pprint(response.text)
